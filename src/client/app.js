20528533d38bfb4b2ea5d54a9f5c657226d06c9b
Chad Martin <Chad Martin>
2017-04-25 14:45:52 -0700

    js-deploy-ui heroku

---
+++ src/client/app.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-debugger */
+/** Main Client Module Loader for basic SPA
+/* create the main angular app module and config*/
+//import in styles
+import './public/styles/styles.less';
+//import in images
+//import bootstrap and font awesome
+
+import angular from 'angular';
+import 'angular-resource';
+import 'angular-ui-router';
+//load movie app resources
+import {default as _movieService} from './app/services/service';
+import {default as  _movieControllers} from './app/controllers/controller';
+import {default as _movieInfo} from './app/widgets/movieInfo';
+import routes from  './app/router/uiroute';
+let app = angular.module('movieApp',['ui.router','ngResource']).config(routes);
+
+  _movieService(app);
+  _movieControllers(app);
+
+
+//load movie app resources
---
+++ src/client/app/services/service.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+/** Movie service
+*   This class will be where the you put Client side services
+*
+*/
+import getBaseUrl from '../../../../src/server/core/baseUrl'
+let baseUrl = getBaseUrl();
+let movieService = (app)=>
+{
+    app.factory('movieService',['$resource', ($resource) =>
+    {
+      class MovieService {
+          constructor($resource) {
+
+              this.$resource = $resource;
+              this.MovieResource = $resource(`${baseUrl}/movies/:id`);
+          }
+          saveMovie(movie) {
+              return this.MovieResource.save(movie).$promise;
+          }
+          getMovies() {
+              return this.MovieResource.query().$promise;
+          }
+          get(id) {
+              return this.MovieResource.get({ id: id });
+          }
+          deleteMovie(id) {
+              return this.MovieResource.delete({ id: id }).$promise;
+          }
+        }
+      return new MovieService($resource);
+      }]);
+};
+export default movieService;
